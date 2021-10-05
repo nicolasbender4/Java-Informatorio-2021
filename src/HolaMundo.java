e9dcf1fffac4aa466a73e60bfdb7008166e7f33c
Nicolás Bender <nicolasbender@gmail.com>
2021-10-04 23:41:49 -0300

    Subi Hola Mundo

---
+++ src/HolaMundo.java
@@ -0,0 +1,16 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        byte numeroByte;
+        short numeroShort;
+        int numeroEntero;
+        long numeroLong;
+        char letra = 'A';
+        float real;
+        double realGrande;
+        String cadena = "Esta es una cadena...";
+        System.out.println("La cadena es: " + cadena);
+
+
+    }
+}
