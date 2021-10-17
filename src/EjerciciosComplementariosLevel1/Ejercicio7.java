f76f1f23e95a0b48f95bc549eba7a4ab0914a146
nicolasbender4 <88468324+nicolasbender4@users.noreply.github.com>
2021-10-16 21:53:40 -0300

    Create Ejercicio7.java
    Ejercicios Actualizazos

---
+++ src/EjerciciosComplementariosLevel1/Ejercicio7.java
@@ -0,0 +1,30 @@
+package EjerciciosComplementariosLevel1;
+
+/*
+
+*/
+
+import java.util.Scanner;
+
+public class Ejercicio7 {
+    
+    public static void main(String[] args){
+        //  Ingreso Palabra
+        System.out.println("Ingrese una palabra: ");
+        Scanner entrada = new Scanner(System.in);
+        String palabra = entrada.nextLine();
+        entrada.close();
+        //  Separo en un arreglo las letras
+        char[] letras = palabra.toCharArray();
+
+        for (char i : letras) {
+            int valor = (int) i;
+            if (valor >= 97 && valor < 122){
+                valor = valor - 32;
+                System.out.println((char) valor);
+            } else{
+                System.out.println((char) valor);
+            }
+        }
+    }
+}
